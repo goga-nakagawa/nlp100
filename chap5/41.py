ec707b60c92d026b1f5975ff00704c63e0013c0a
nakagawahiroshi <nakagawa@goga.co.jp>
2016-07-26 15:28:09 +0900

    added 41.py

---
+++ chap5/41.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+eos = re.compile(r"^EOS")
+astrisk = re.compile(r"^\*(.*?)")
+parse = re.compile(r"\t|\,")
+
+
+class Morph:
+    def __init__(self, surface=None, base=None, pos=None, pos1=None):
+        self.surface = surface
+        self.base = base
+        self.pos = pos
+        self.pos1 = pos1
+
+    def __str__(self):
+        return "surface: %s, base: %s, pos: %s, pos1: %s" % (self.surface, self.base, self.pos, self.pos1)
+
+
+class Chunk:
+    def __init__(self, dst=None, srcs=None):
+        self.morphs = []
+        self.dst = int(dst.strip("D"))
+        self.srcs = int(srcs)
+
+    def __str__(self):
+        return "dst: %s, srcs: %s" % (self.dst, self.srcs)
+
+    def add_morph(self, Morph):
+        self.morphs.append(Morph)
+
+
+sentences = []
+sentence = []
+chunk = None
+
+with open('neko.txt.cabocha') as f:
+
+    for line in f.readlines():
+        eosed = eos.match(line)
+        astrisked = astrisk.match(line)
+        parsed = parse.split(line)
+
+        if eosed:
+            sentences.append(sentence)
+            sentence = []
+        elif astrisked:
+            kakari = line.split()
+            chunk = Chunk(
+                        dst=kakari[2],
+                        srcs=kakari[1]
+                )
+            sentence.append(chunk)
+        elif parsed and len(parsed) == 10:
+            morph = Morph(
+                        surface=parsed[0],
+                        base=parsed[7],
+                        pos=parsed[1],
+                        pos1=parsed[2]
+                    )
+            chunk.add_morph(morph)
+
+
+for chunk in sentences[9]:
+    print chunk
+    for morph in chunk.morphs:
+        print morph
